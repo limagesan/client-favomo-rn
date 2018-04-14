c60887dfef9447b8e48d94c2e28b6bec0112a148
limagesan <limagesan@gmail.com>
2018-04-14 18:23:00 +0900

    feat: implement Tab Navigation(WIP)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React, { Component } from "react";
 
-import { StackNavigator } from "react-navigation";
+import { StackNavigator, TabNavigator, TabBarBottom } from "react-navigation";
 
 import { Color } from "./styles";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "./HomeScreen";
 import PostScreen from "./PostScreen";
+import FeedScreen from "./FeedScreen";
 
-export default class App extends Component {
-  render() {
-    return <RootStack />;
+// export default class App extends Component {
+//   render() {
+//     return <RootTab />;
+//   }
+// }
+
+const HomeStack = StackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Post: {
+    screen: PostScreen
   }
-}
+});
 
-const RootStack = StackNavigator(
+const FeedStack = StackNavigator({
+  Feed: {
+    screen: FeedScreen
+  }
+});
+
+export default TabNavigator(
   {
-    Home: {
-      screen: HomeScreen
-    },
-    Post: {
-      screen: PostScreen
-    }
+    Home: { screen: HomeStack },
+    Feed: { screen: FeedStack }
   },
   {
-    initialRouteName: "Home",
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: Color.base
-      },
-      headerTintColor: "black",
-      headerTitleStyle: {
-        fontWeight: "bold"
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options${focused ? "" : "-outline"}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
       }
-    }
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    },
+    animationEnabled: false,
+    swipeEnabled: false
   }
 );
