7802652e76d9e4327ccd6abf12f78b3522286327
limagesan <limagesan@gmail.com>
2018-04-30 22:39:50 +0900

    ios, android, package.jsonを初期状態に戻した

--- android/app/src/main/java/com/clientfavomorn/MainApplication.java
+++ android/app/src/main/java/com/clientfavomorn/MainApplication.java
@@ -3,24 +3,6 @@ package com.clientfavomorn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -40,25 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage(),
-            new PickerPackage(),
-            new PickerPackage(),
-            new PickerPackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage(),
-            new RNFirebasePackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage(),
-            new RNFirebasePackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage(),
-            new RNFirebasePackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage(),
-            new RNFirebasePackage(),
-            new VectorIconsPackage(),
-            new NavigationReactPackage()
+          new MainReactPackage()
       );
     }
 
--- ios/clientfavomorn/AppDelegate.m
+++ ios/clientfavomorn/AppDelegate.m
@@ -11,14 +11,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import <Firebase.h>
-#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
@@ -35,21 +32,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-  return [RCTLinkingManager application:application openURL:url options:options];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
- restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
-{
-  return [RCTLinkingManager application:application
-                   continueUserActivity:userActivity
-                     restorationHandler:restorationHandler];
 }
 
 @end
