beb99bf529d58c6eafed52e09b102e0108da0276
limagesan <limagesan@users.noreply.github.com>
2018-05-10 21:03:18 +0900

    feat: SharedContents コンポーネントを作成

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -21,7 +21,6 @@ import { logout } from '../actions';
 
 const db = firebase.firestore();
 
-
 class Feed extends Component {
   static navigationOptions = {
     title: 'Feed',
@@ -39,7 +38,7 @@ class Feed extends Component {
 
   onPressButton = () => {
     this.props.navigation.goBack();
-  }
+  };
 
   onRefresh = () => {
     this.setState({ refreshing: true });
@@ -313,29 +312,6 @@ class MyListItem extends React.PureComponent {
 
   render() {
     const { item } = this.props;
-    const { shortIconRef } = item.data;
-
-    let favicon = '../assets/spotify.png';
-    // http://www.ietf.org/rfc/rfc3986.txt 50p参照
-    const urlRegexp = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/i;
-
-    if (shortIconRef) {
-      let matches = shortIconRef.match(/^https|^http/);
-      if (matches && matches.length > 0) {
-        favicon = shortIconRef;
-      }
-      matches = shortIconRef.match(/^\/\//);
-      if (matches && matches.length > 0) {
-        const matches2 = item.url.match(urlRegexp);
-        favicon = `${matches2[1]}${shortIconRef}`;
-      }
-      matches = shortIconRef.match(/^\/[^/]/);
-      if (matches && matches.length > 0) {
-        const matches2 = item.url.match(urlRegexp);
-        favicon = `${matches2[1]}${matches2[3]}${shortIconRef}`;
-      }
-    }
-    console.log('favicon', favicon);
 
     return (
       <View
@@ -345,64 +321,7 @@ class MyListItem extends React.PureComponent {
           height: 190,
         }}
       >
-        <View style={{ flexDirection: 'row' }}>
-          <View style={{ flex: 4 }}>
-            <Image
-              style={{
-                width: 94,
-                height: 94,
-                borderRadius: 10,
-              }}
-              source={{ uri: item.data.image }}
-            />
-          </View>
-          <View style={{ paddingLeft: 10, flex: 11 }}>
-            <TouchableOpacity onPress={this.onPress}>
-              <View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    overflow: 'hidden',
-                    alignItems: 'center',
-                  }}
-                  removeClippedSubviews
-                >
-                  <Image
-                    style={{
-                      width: 15,
-                      height: 15,
-                    }}
-                    source={{ uri: favicon }}
-                  />
-                  <Text
-                    style={{
-                      flex: 1,
-                      marginLeft: 5,
-                      fontSize: 13,
-                    }}
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                  >
-                    {item.url}
-                  </Text>
-                </View>
-                <Text
-                  style={{
-                    marginTop: 7,
-                    fontSize: 15,
-                    fontWeight: 'bold',
-                  }}
-                  numberOfLines={2}
-                >
-                  {item.data.title}
-                </Text>
-                <Text style={{ fontSize: 11 }} numberOfLines={3}>
-                  {item.data.description}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <SharedContents contents={item.data} onPress={this.onPress} />
         <View
           style={{
             flexDirection: 'row',
@@ -472,6 +391,92 @@ class MyListItem extends React.PureComponent {
     );
   }
 }
+
+const SharedContents = ({ contents, onPress }) => {
+  const { shortIconRef } = contents;
+
+  let favicon = '../assets/spotify.png';
+  // http://www.ietf.org/rfc/rfc3986.txt 50p参照
+  const urlRegexp = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/i;
+
+  if (shortIconRef) {
+    let matches = shortIconRef.match(/^https|^http/);
+    if (matches && matches.length > 0) {
+      favicon = shortIconRef;
+    }
+    matches = shortIconRef.match(/^\/\//);
+    if (matches && matches.length > 0) {
+      const matches2 = contents.url.match(urlRegexp);
+      favicon = `${matches2[1]}${shortIconRef}`;
+    }
+    matches = shortIconRef.match(/^\/[^/]/);
+    if (matches && matches.length > 0) {
+      const matches2 = contents.url.match(urlRegexp);
+      favicon = `${matches2[1]}${matches2[3]}${shortIconRef}`;
+    }
+  }
+  console.log('favicon', favicon);
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <View style={{ flex: 4 }}>
+        <Image
+          style={{
+            width: 94,
+            height: 94,
+            borderRadius: 10,
+          }}
+          source={{ uri: contents.image }}
+        />
+      </View>
+      <View style={{ paddingLeft: 10, flex: 11 }}>
+        <TouchableOpacity onPress={onPress}>
+          <View>
+            <View
+              style={{
+                flexDirection: 'row',
+                overflow: 'hidden',
+                alignItems: 'center',
+              }}
+              removeClippedSubviews
+            >
+              <Image
+                style={{
+                  width: 15,
+                  height: 15,
+                }}
+                source={{ uri: favicon }}
+              />
+              <Text
+                style={{
+                  flex: 1,
+                  marginLeft: 5,
+                  fontSize: 13,
+                }}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {contents.url}
+              </Text>
+            </View>
+            <Text
+              style={{
+                marginTop: 7,
+                fontSize: 15,
+                fontWeight: 'bold',
+              }}
+              numberOfLines={2}
+            >
+              {contents.title}
+            </Text>
+            <Text style={{ fontSize: 11 }} numberOfLines={3}>
+              {contents.description}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const mapStateToProps = state => ({ ...state });
 
