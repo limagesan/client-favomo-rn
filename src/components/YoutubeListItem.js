63c36559cf38bf695a7bfdbd4460cd5d601bde39
limagesan <limagesan@users.noreply.github.com>
2018-05-10 21:57:40 +0900

    chore: YoutubeListItem コンポーネントを作成

---
+++ src/components/YoutubeListItem.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
+import YouTube from 'react-native-youtube';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+class YoutubeListItem extends React.PureComponent {
+  onPress = () => {
+    console.log('pressed');
+  };
+
+  render() {
+    const { item } = this.props;
+    const { url } = item;
+    let id = '';
+    if (url.indexOf('youtube.com') >= 0) {
+      const matches = url.match(/watch\?v=.{11}/);
+      id = matches[0].match(/.{11}$/);
+    } else if (url.indexOf('youtu.be') >= 0) {
+      const matches = url.match(/youtu\.be\/.{11}/);
+      id = matches[0].match(/.{11}$/);
+    }
+    if (id.length > 0) {
+      [id] = id;
+    }
+
+    const { width } = Dimensions.get('window');
+    let height = width * 9;
+    height /= 16;
+
+    return (
+      <View
+        style={{
+          borderTopWidth: 1,
+          height: height + 90,
+          width,
+        }}
+      >
+        <YouTube
+          videoId={id} // The YouTube video ID
+          play={false} // control playback of video with true/false
+          fullscreen={false} // control whether the video should play in fullscreen or inline
+          loop={false} // control whether the video should loop when ended
+          onReady={e => console.log('Youtube is Ready', e)}
+          onChangeState={e => console.log('Youtube ChangeState', e.state)}
+          onChangeQuality={e => console.log('Youtube ChangeQuality', e.quality)}
+          onError={e => console.log('Youtube Error', e.error)}
+          style={{
+            height,
+            width,
+            alignSelf: 'stretch',
+          }}
+        />
+        <View
+          style={{
+            marginTop: 10,
+            padding: 10,
+          }}
+        >
+          <View style={{ flexDirection: 'row' }}>
+            <Image
+              style={{
+                width: 35,
+                height: 35,
+                borderRadius: 17.5,
+              }}
+              source={{ uri: item.poster.thumbIconURL }}
+            />
+            <View
+              style={{
+                marginLeft: 10,
+                flexDirection: 'row',
+                flex: 1,
+              }}
+            >
+              <View style={{ flex: 4 }}>
+                <Text style={{ fontSize: 10 }}>{item.poster.id}</Text>
+                <Text style={{ marginTop: 5 }}>{item.caption}</Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'flex-end',
+              alignItems: 'flex-end',
+              paddingRight: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                console.log('tap comment');
+              }}
+            >
+              <Text style={{ fontSize: 14 }}>コメントする</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                console.log('liked');
+              }}
+              style={{ marginLeft: 20 }}
+            >
+              <Icon name="thumbs-o-up" size={28} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                console.log('smiled');
+              }}
+            >
+              <Image
+                style={{
+                  marginLeft: 10,
+                  width: 25,
+                  height: 25,
+                }}
+                source={require('../assets/happiness.png')}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default YoutubeListItem;
--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -7,17 +7,16 @@ import {
   ScrollView,
   Image,
   RefreshControl,
-  Dimensions,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { OpenGraphParser } from 'react-native-opengraph-kit';
 
 import SafariView from 'react-native-safari-view';
 import firebase from 'react-native-firebase';
-import YouTube from 'react-native-youtube';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 import SharedContents from '../components/SharedContents';
+import YoutubeListItem from '../components/YoutubeListItem';
 import { logout } from '../actions';
 
 const db = firebase.firestore();
@@ -137,7 +136,7 @@ class MultiSelectList extends React.PureComponent {
   renderItem = ({ item }) => {
     const ListItem =
       item.url.indexOf('youtube.com') >= 0 || item.url.indexOf('youtu.be') >= 0 ? (
-        <YoutubeItem id={item.id} item={item} />
+        <YoutubeListItem id={item.id} item={item} />
       ) : (
         <MyListItem
           id={item.id}
@@ -187,124 +186,6 @@ class MultiSelectList extends React.PureComponent {
   }
 }
 
-class YoutubeItem extends React.PureComponent {
-  onPress = () => {
-    console.log('pressed');
-  };
-
-  render() {
-    const { item } = this.props;
-    const { url } = item;
-    let id = '';
-    if (url.indexOf('youtube.com') >= 0) {
-      const matches = url.match(/watch\?v=.{11}/);
-      id = matches[0].match(/.{11}$/);
-    } else if (url.indexOf('youtu.be') >= 0) {
-      const matches = url.match(/youtu\.be\/.{11}/);
-      id = matches[0].match(/.{11}$/);
-    }
-    if (id.length > 0) {
-      [id] = id;
-    }
-
-    const { width } = Dimensions.get('window');
-    let height = width * 9;
-    height /= 16;
-
-    return (
-      <View
-        style={{
-          borderTopWidth: 1,
-          height: height + 90,
-          width,
-        }}
-      >
-        <YouTube
-          videoId={id} // The YouTube video ID
-          play={false} // control playback of video with true/false
-          fullscreen={false} // control whether the video should play in fullscreen or inline
-          loop={false} // control whether the video should loop when ended
-          onReady={e => this.setState({ isReady: true })}
-          onChangeState={e => this.setState({ status: e.state })}
-          onChangeQuality={e => this.setState({ quality: e.quality })}
-          onError={e => this.setState({ error: e.error })}
-          style={{
-            height,
-            width,
-            alignSelf: 'stretch',
-          }}
-        />
-        <View
-          style={{
-            marginTop: 10,
-            padding: 10,
-          }}
-        >
-          <View style={{ flexDirection: 'row' }}>
-            <Image
-              style={{
-                width: 35,
-                height: 35,
-                borderRadius: 17.5,
-              }}
-              source={{ uri: item.poster.thumbIconURL }}
-            />
-            <View
-              style={{
-                marginLeft: 10,
-                flexDirection: 'row',
-                flex: 1,
-              }}
-            >
-              <View style={{ flex: 4 }}>
-                <Text style={{ fontSize: 10 }}>{item.poster.id}</Text>
-                <Text style={{ marginTop: 5 }}>{item.caption}</Text>
-              </View>
-            </View>
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'flex-end',
-              alignItems: 'flex-end',
-              paddingRight: 10,
-            }}
-          >
-            <TouchableOpacity
-              onPress={() => {
-                console.log('tap comment');
-              }}
-            >
-              <Text style={{ fontSize: 14 }}>コメントする</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                console.log('liked');
-              }}
-              style={{ marginLeft: 20 }}
-            >
-              <Icon name="thumbs-o-up" size={28} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                console.log('smiled');
-              }}
-            >
-              <Image
-                style={{
-                  marginLeft: 10,
-                  width: 25,
-                  height: 25,
-                }}
-                source={require('../assets/happiness.png')}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    );
-  }
-}
 
 class MyListItem extends React.PureComponent {
   onPress = () => {
